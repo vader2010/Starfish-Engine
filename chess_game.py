a460b2a7c365045645c02367f040d4335cbdf29a
Michael Cutsail <65092568+vader2010@users.noreply.github.com>
2023-07-17 17:19:01 -0700

    Update chess_game.py

--- chess_game.py
+++ chess_game.py
@@ -7,30 +7,31 @@ pygame.init()
 app = pygame.display.set_mode((512, 512))
 pygame.display.set_caption("Chess with AI")
 
-
 board = chess.Board()
-board1 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\board1.png").convert()
-board2 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\board2.png").convert()
-pawn = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\pawn.png").convert()
-pawn2 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\pawn2.png").convert()
-knight = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\knight.png").convert()
-knight2 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\knight2.png").convert()
-bishop = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\bishop.png").convert()
-bishop2 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\bishop2.png").convert()
-rook = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\rook.png").convert()
-rook2 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\rook2.png").convert()
-queen = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\queen.png").convert()
-queen2 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\queen2.png").convert()
-king = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\king.png").convert()
-king2 = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Chess Engine\\pieces\\king2.png").convert()
+board.apply_mirror()
+board1 = pygame.transform.scale(pygame.image.load("board1.png").convert_alpha(), (64, 64))
+board2 = pygame.transform.scale(pygame.image.load("board2.png").convert_alpha(), (64, 64))
+pawn = pygame.transform.scale(pygame.image.load("pawn.png").convert_alpha(), (64, 64))
+pawn2 = pygame.transform.scale(pygame.image.load("pawn2.png").convert_alpha(), (64, 64))
+knight = pygame.transform.scale(pygame.image.load("knight.png").convert_alpha(), (64, 64))
+knight2 = pygame.transform.scale(pygame.image.load("knight2.png").convert_alpha(), (64, 64))
+bishop = pygame.transform.scale(pygame.image.load("bishop.png").convert_alpha(), (64, 64))
+bishop2 = pygame.transform.scale(pygame.image.load("bishop2.png").convert_alpha(), (64, 64))
+rook = pygame.transform.scale(pygame.image.load("rook.png").convert_alpha(), (64, 64))
+rook2 = pygame.transform.scale(pygame.image.load("rook2.png").convert_alpha(), (64, 64))
+queen = pygame.transform.scale(pygame.image.load("queen.png").convert_alpha(), (64, 64))
+queen2 = pygame.transform.scale(pygame.image.load("queen2.png").convert_alpha(), (64, 64))
+king = pygame.transform.scale(pygame.image.load("king.png").convert_alpha(), (64, 64))
+king2 = pygame.transform.scale(pygame.image.load("king2.png").convert_alpha(), (64, 64))
 
 currentx = 0
 currenty = 0
 
+
 def draw_board():
     for i in range(4):
         currentx = 0
-        currenty = i*128
+        currenty = i * 128
         index = 0
         for i2 in range(0, 16):
             index += 1
@@ -44,46 +45,81 @@ def draw_board():
                 app.blit(board2, (currentx, currenty))
             currentx += 64
 
-def update_chess_pieces():
-    currentx = 0
-    currenty = 0
-    board2 = str(board).replace(" ", "").split("\n")
-    for i in range(8):
-        currentx = 0
-        for i2 in range(8):
-            if board2[i][i2] == "p":
-                app.blit(pawn2, (currentx, currenty))
-            if board2[i][i2] == "P":
-                app.blit(pawn, (currentx, currenty))
-            if board2[i][i2] == "n":
-                app.blit(knight2, (currentx, currenty))
-            if board2[i][i2] == "N":
-                app.blit(knight, (currentx, currenty))
-            if board2[i][i2] == "b":
-                app.blit(bishop2, (currentx, currenty))
-            if board2[i][i2] == "B":
-                app.blit(bishop, (currentx, currenty))
-            if board2[i][i2] == "r":
-                app.blit(rook2, (currentx, currenty))
-            if board2[i][i2] == "R":
-                app.blit(rook, (currentx, currenty))
-            if board2[i][i2] == "q":
-                app.blit(queen2, (currentx, currenty))
-            if board2[i][i2] == "Q":
-                app.blit(queen, (currentx, currenty))
-            if board2[i][i2] == "k":
-                app.blit(king2, (currentx, currenty))
-            if board2[i][i2] == "K":
-                app.blit(king, (currentx, currenty))
-        currenty += 64
 
+def update_chess_pieces():
+    for i in range(0, 64):
+        piece = board.piece_at(i)
+        if piece is not None:
+            piece = piece.symbol()
+            position = ((i % 8) * 64, (i // 8) * 64)
+            if piece.upper() == "K":
+                if board.color_at(i) == chess.BLACK:
+                    app.blit(king, position)
+                else:
+                    app.blit(king2, position)
+            if piece.upper() == "Q":
+                if board.color_at(i) == chess.BLACK:
+                    app.blit(queen, position)
+                else:
+                    app.blit(queen2, position)
+            if piece.upper() == "R":
+                if board.color_at(i) == chess.BLACK:
+                    app.blit(rook, position)
+                else:
+                    app.blit(rook2, position)
+            if piece.upper() == "B":
+                if board.color_at(i) == chess.BLACK:
+                    app.blit(bishop, position)
+                else:
+                    app.blit(bishop2, position)
+            if piece.upper() == "N":
+                if board.color_at(i) == chess.BLACK:
+                    app.blit(knight, position)
+                else:
+                    app.blit(knight2, position)
+            if piece.upper() == "P":
+                if board.color_at(i) == chess.BLACK:
+                    app.blit(pawn, position)
+                else:
+                    app.blit(pawn2, position)
+        else:
+            continue
+
+def get_square_from_click(x, y):
+    if x > 512 or x < 0 or y > 512 or y < 0:
+        return None
+    rows = list("abcdefgh")
+    square = f"{rows[x // 64]}{(y // 64) + 1}"
+    return square
+
+
+turn = False
+new_move = True
+move = []
 running = True
 while running:
+    if new_move:
+        if turn is True:
+            turn = False
+        else:
+            turn = True
+        new_move = False
+    app.fill((0, 0, 0))
     draw_board()
     update_chess_pieces()
-    pygame.display.update()
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                x, y = event.pos
+                move.append(get_square_from_click(x, y))
+                if len(move) == 2:
+                    move2 = chess.Move.from_uci(f"{move[0]}{move[1]}")
+                    if board.is_legal(chess.Move.from_uci(f"{move[0]}{move[1]}")):
+                        new_move = True
+                        board.push(move2)
+                    move = []
 
 pygame.quit()
